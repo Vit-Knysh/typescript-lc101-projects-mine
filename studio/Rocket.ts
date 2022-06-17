fdd925a8808d710ec20d7041002aae0e13d8fe56
Vit-Knysh <v,knysh85@gmail.com>
2022-06-16 22:48:15 -0400

    Added to Roccket class constructor and sumMass

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,3 +1,24 @@
-export class Rocket {
+import { Payload } from './Payload';
+import { Cargo } from './Cargo';
+import { Astronaut } from './Astronaut';
+export class Rocket implements Payload {
+    name: string;
+    massKg : number;
+    totalCapacityKg : number;
+    cargoItems : Cargo [] = [];
+    astronauts : Astronaut [] = [];
 
+    constructor (name: string, totalCapacityKg: number, ) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    };
+
+    sumMass (items : Payload []) : number {
+        let sum : number = 0;
+        for (let i = 0; i < items.length; i++) {
+            sum += items[i].massKg;
+        };
+        return sum;
+
+    }
 };
